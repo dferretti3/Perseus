9f7d7ad09284571d1248c4bb2fde3308905d1639
Arlen Chen <iotarlen@gatech.edu>
2012-04-20 15:08:45 -0400

    ai machine gun

--- Desktop/Perseus/unity/Assets/Scripts/AIControl.cs
+++ Desktop/Perseus/unity/Assets/Scripts/AIControl.cs
@@ -6,6 +6,8 @@ public class AIControl : MonoBehaviour {
 	public Bullet b;
 	int count;
 	int lead;
+	int spread;
+	GameObject target;
 	// Use this for initialization
 	void Start () {
 	
@@ -18,20 +20,32 @@ public class AIControl : MonoBehaviour {
 		foreach (Collider hit in cols){
 			if((hit.tag == "P1" || hit.tag == "P2"))
 			{
-				Vector3 distance = transform.position - hit.gameObject.transform.position;
-				float d = distance.sqrMagnitude;
-				print(d);
-				if(d<=20000)
-					lead = 10;
-				if(d>20000)
-					lead = 40;
-				transform.LookAt(hit.gameObject.transform.position+hit.gameObject.transform.forward*lead);
-				if(count%20==0)
-				{
-					transform.LookAt(hit.gameObject.transform.position+hit.gameObject.transform.forward*lead-new Vector3(Random.Range(-5,5),Random.Range(-5,5),Random.Range(-5,5)));
-					Network.Instantiate(b, transform.position+transform.forward*20, transform.rotation, 0);
-				}
+				target = hit.gameObject;
+				break;
+			}	
+		}
+		if(target!=null)
+		{
+			Vector3 distance = transform.position - target.transform.position;
+			float d = distance.sqrMagnitude;
+			print(d);
+			if(d<=20000)
+			{
+				lead = 10;
+				spread = 2;
+			}
+			if(d>20000)
+			{
+				lead = 40;
+				spread = 5;
+			}
+			transform.LookAt(target.transform.position+target.transform.forward*lead);
+			if(count%20==0)
+			{
+				transform.LookAt(target.transform.position+target.transform.forward*lead-new Vector3(Random.Range(-spread,spread),Random.Range(-spread,spread),Random.Range(-spread,spread)));
+				Network.Instantiate(b, transform.position+transform.forward*20, transform.rotation, 0);
 			}
 		}
 	}
 }
+
