a80506e6186fa6b0949fced0bd6834831834ae9e
David Ferretti lol <dferretti3@gatech.edu>
2012-04-24 10:57:49 -0400

    LAST

--- Desktop/Perseus/unity/Assets/Scripts/Bomb.cs
+++ Desktop/Perseus/unity/Assets/Scripts/Bomb.cs
@@ -72,18 +72,64 @@ public class Bomb : MonoBehaviour {
 				engine.enableEmission = false;
 			}
 			float explosionRad = 10;
-			int halfHit = 10;
-//			Collider[] hitTurretts = Physics.OverlapSphere(transform.position,explosionRad,1<<10);
-//			foreach(Collider turrett in hitTurretts)
-//			{
-//				topLevelController ttlc = turrett.transform.GetComponentInChildren<topLevelController>();
-//				int hitFor = (int)(explosionRad - (turrett.transform.position - transform.position).magnitude)*halfHit + halfHit;
-//				turrett.networkView.RPC("hitTower",RPCMode.All,hitFor);
-//			}
+			int halfHit = 20;
+			Collider[] hitTurretts = Physics.OverlapSphere(transform.position,explosionRad,1<<10);
+			foreach(Collider turrett in hitTurretts)
+			{
+				topLevelController ttlc = turrett.transform.GetComponentInChildren<topLevelController>();
+				int hitFor = (int)(explosionRad - (turrett.transform.position - transform.position).magnitude)*halfHit + halfHit;
+				turrett.networkView.RPC("hitTower",RPCMode.All,hitFor);
+			}
 //        	Network.Destroy(gameObject);
 //			GameObject explode = (GameObject)Network.Instantiate(explosionRadius,transform.position,Quaternion.identity,0);
 //			explode.transform.localScale *= 5;
+			Vector3 pos = transform.position;
 			Network.Destroy(gameObject);
+			Explode(pos);
+		}
+	}
+	
+	public void Explode(Vector3 pos)
+	{
+		float radius = 22;
+		Collider[] projectiles = Physics.OverlapSphere(pos,radius,1<<13);
+		foreach (Collider proj in projectiles)
+		{
+			if (proj.gameObject==gameObject || !proj.gameObject.active) continue;
+			kill(proj.gameObject);
+		}
+		Network.Instantiate(explosionRadius,pos,Quaternion.identity,0);
+	}
+	
+	void kill(GameObject g)
+	{
+		if (g.GetComponent<AIControlledMissile>()!=null) {
+			g.GetComponent<AIControlledMissile>().gameObject.active = false;
+			g.GetComponent<AIControlledMissile>().kill();
+		}
+		else if (g.GetComponent<AIresourceRobot>()!=null) {
+			g.GetComponent<AIresourceRobot>().gameObject.active = false;
+			g.GetComponent<AIresourceRobot>().kill();
+		}
+		else if (g.GetComponent<Bomb>()!=null) {
+			g.GetComponent<Bomb>().gameObject.active = false;
+			g.GetComponent<Bomb>().kill();
+		}
+		else if (g.GetComponent<Bullet>()!=null) {
+			if (g.networkView.viewID.owner==Network.player)
+				Network.Destroy(g);
+		}
+		else if (g.GetComponent<ControlledMissile>()!=null) {
+			g.GetComponent<ControlledMissile>().gameObject.active = false;
+			g.GetComponent<ControlledMissile>().kill();
+		}
+		else if (g.GetComponent<homingMissileScript>()!=null) {
+			g.GetComponent<homingMissileScript>().gameObject.active = false;
+			g.GetComponent<homingMissileScript>().kill();
+		}
+		else if (g.GetComponent<resourceRobot>()!=null) {
+			g.GetComponent<resourceRobot>().gameObject.active = false;
+			g.GetComponent<resourceRobot>().kill();
 		}
 	}
 	
